0b5a10c43ffdc46379ef74150023d05f59ac736c
catsarebetter122 <catsarebetter122@gmail.com>
2025-08-20 14:49:32 -0400

    Configure Flask Project for Bolt

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from app import app
 import routes  # noqa: F401
 import os
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
     app.run(host="0.0.0.0", port=port, debug=True)
